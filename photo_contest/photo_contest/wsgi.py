4c160b3aa8f2a93de58ad43c9f697d18ce845979
Waytoobig <chukwuderanwakwo@gmail.com>
2025-12-01 22:05:31 +0100

    yytuu

--- photo_contest/photo_contest/wsgi.py
+++ photo_contest/photo_contest/wsgi.py
@@ -15,5 +15,6 @@ _static_root = Path(__file__).resolve().parent.parent / "staticfiles"
 _static_root.mkdir(parents=True, exist_ok=True)
 
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photo_contest.photo_contest.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photo_contest.settings')
 application = get_wsgi_application()
+
